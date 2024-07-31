aa445369f0f26fabc1fd355c2c7cb9bacb889d98
VinayAmbatkar <Vinayambatkar57@gmail.com>
2024-07-31 23:53:24 +0530

    GrideButtons Added

--- src/Pages/Listing/index.js
+++ src/Pages/Listing/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Sidebar from '../../Components/Sidebar';
+import Button from '@mui/material/Button';
+import { CiMenuBurger } from "react-icons/ci";
+import { CgMenuGridR } from "react-icons/cg";
+import { PiDotsSixBold } from "react-icons/pi";
+import { HiViewGrid } from "react-icons/hi";
 
 
 const Listing = () => {
@@ -20,7 +25,20 @@ const Listing = () => {
                         <div className="content_right">
 
                            <img src="https://github.com/rinkuv37/fullstack-ecommerce/blob/main/images/slideBanner2.jpg?raw=true" alt="" className='w-100' style={{borderRadius:'8px'}}/> 
+
+                           <div className="showBy mt-3 mb-3 d-flex align-items-center">
+                            <div className=" d-flex align-items-center btnWrapper">
+                                <Button><CiMenuBurger/></Button>
+                                <Button><HiViewGrid  /></Button>
+                                <Button><CgMenuGridR/></Button>
+                                <Button><PiDotsSixBold/></Button>
+                             
+                            </div>
+
+                                
+                           </div>
                         </div>
+                        
                     </div>
 
 
